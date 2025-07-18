602ba1f3d96e0f1aa6a072b16be5bcd34fe9893e
josemaNavNor <navvajosen@gmail.com>
2025-07-18 16:14:01 -0700

    remove unused AppController, AppService, and their tests; update database connection details in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'tu_usuario',
-      password: 'tu_contraseña',
-      database: 'nombre_bd',
+      username: 'JoseNavarro',
+      password: '123456',
+      database: 'db_mefasa_tickets',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       //synchronize: true, // ¡Solo para desarrollo!
     }),
